1120743ca4d642cc3ac036289527461bfa985966
ihsanlearn <iihsannlearn@gmail.com>
2025-06-17 17:45:31 +0700

    main

--- src/Main.java
+++ src/Main.java
@@ -16,21 +16,24 @@ public class Main {
         int choice;
 
         do {
-            System.out.println("==== Daisuke Clinic ====");
-            System.out.println("1. Add New Patient");
-            System.out.println("2. Remove Patient by ID");
-            System.out.println("3. Search Patient by Name");
-            System.out.println("4. Display All Patients");
-            System.out.println("5. Doctor Login");
-            System.out.println("6. Doctor Logout");
-            System.out.println("7. View Last Logged-in Doctor");
-            System.out.println("8. Schedule Appointment");
-            System.out.println("9. Process Appointment");
-            System.out.println("10. Display Upcoming Appointments");
-            System.out.println("11. Search Patient by ID (BST)");
-            System.out.println("12. Display All Patients (BST Inorder)");
-            System.out.println("0. Exit");
-            System.out.print("Choose: ");
+            System.out.println("[[===============================================]]");
+            System.out.println("[[ Clinic Sehat Tentram Sejahtera Indonesia Raya ]]");
+            System.out.println("[[===============================================]]");
+            System.out.println("[[ ( 1 ) Add New Patient                         ]]");
+            System.out.println("[[ ( 2 ) Remove Patient by ID                    ]]");
+            System.out.println("[[ ( 3 ) Search Patient by Name                  ]]");
+            System.out.println("[[ ( 4 ) Display All Patients                    ]]");
+            System.out.println("[[ ( 5 ) Doctor Login                            ]]");
+            System.out.println("[[ ( 6 ) Doctor Logout                           ]]");
+            System.out.println("[[ ( 7 ) View Last Logged-in Doctor              ]]");
+            System.out.println("[[ ( 8 ) Schedule Appointment                    ]]");
+            System.out.println("[[ ( 9 ) Process Appointment                     ]]");
+            System.out.println("[[ ( 10) Display Upcoming Appointments           ]]");
+            System.out.println("[[ ( 11) Search Patient by ID (BST)              ]]");
+            System.out.println("[[ ( 12) Display All Patients (BST Inorder)      ]]");
+            System.out.println("[[ ( 0 ) Exit                                    ]]");
+            System.out.println("[[-----------------------------------------------]]");
+            System.out.print("]] Choose ?> ");
             choice = Integer.parseInt(sc.nextLine());
 
             switch (choice) {
